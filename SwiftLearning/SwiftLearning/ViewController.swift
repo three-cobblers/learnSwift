9145cc9c11d0d097f8d9970502a7077f7e5a251c
BingGe123 <mrli_bing@163.com>
2020-01-13 18:24:29 +0800

    这样吧

--- SwiftLearning/SwiftLearning/ViewController.swift
+++ SwiftLearning/SwiftLearning/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setUI()
+        
     }
 
     var 王凯荣 = "贱人王"
